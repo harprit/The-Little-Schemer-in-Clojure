2ec80c75bdc1fc9e5bbba264d0bf38f978e8c34b
emanon001 <emanon001@gmail.com>
2012-07-21 08:32:14 +0900

    Added the chapter 5.

---
+++ src/the_little_schemer/5_it_is_full_of_stars.clj
@@ -0,0 +1,102 @@
+(ns the-little-schemer.5-it-is-full-of-stars
+  (:use the-little-schemer.0-preparation
+        [the-little-schemer.4-numbers-games :only (eqan?)]))
+
+(declare eqlist? equal?)
+
+(defn rember*
+  [a ls]
+  (cond
+    (empty? ls) '()
+    (atom? (first ls)) (cond
+                         (= (first ls) a) (rember* a (rest ls))
+                         :else (cons (first ls)
+                                     (rember* a (rest ls))))
+    :else (cons (rember* a (first ls))
+                (rember* a (rest ls)))))
+
+(defn insertR*
+  [new old ls]
+  (cond
+    (empty? ls) '()
+    (atom? (first ls)) (cond
+                         (= (first ls) old) (cons old
+                                                  (cons new
+                                                        (insertR* new old (rest ls))))
+                         :else (cons (first ls)
+                                     (insertR* new old (rest ls))))
+    :else (cons (insertR* new old (first ls))
+                (insertR* new old (rest ls)))))
+
+(defn occur*
+  [a ls]
+  (cond
+    (empty? ls) 0
+    (atom? (first ls)) (cond
+                         (= (first ls) a) (inc (occur* a (rest ls)))
+                         :else (occur* a (rest ls)))
+    :else (+ (occur* a (first ls))
+             (occur* a (rest ls)))))
+
+(defn subst*
+  [new old ls]
+  (cond
+    (empty? ls) '()
+    (atom? (first ls)) (cond
+                         (= (first ls) old) (cons new
+                                                  (subst* new old (rest ls)))
+                         :else (cons (first ls)
+                                     (subst* new old (rest ls))))
+    :else (cons (subst* new old (first ls))
+                (subst* new old (rest ls)))))
+
+(defn insertL*
+  [new old ls]
+  (cond
+    (empty? ls) '()
+    (atom? (first ls)) (cond
+                         (= (first ls) old) (cons new
+                                                  (cons old
+                                                        (insertL* new old (rest ls))))
+                         :else (cons (first ls) (insertL* new old (rest ls))))
+    :else (cons (insertL* new old (first ls))
+                (insertL* new old (rest ls)))))
+
+(defn member*
+  [a ls]
+  (cond
+    (empty? ls) false
+    (atom? (first ls)) (or (= (first ls) a)
+                           (member* a (rest ls)))
+    :else (or (member* a (first ls))
+              (member* a (rest ls)))))
+
+(defn leftmost
+  [ls]
+  (cond
+    (atom? (first ls)) (first ls)
+    :else (leftmost (first ls))))
+
+(defn eqlist?
+  [l1 l2]
+  (cond
+    (and (empty? l1) (empty? l2)) true
+    (or (empty? l1) (empty? l2)) false
+    :else (and (equal? (first l1) (first l2))
+               (eqlist? (rest l1) (rest l2)))))
+
+; Clojure では = を使えばよい
+(defn equal?
+  [s1 s2]
+  (cond
+    (and (atom? s1) (atom? s2)) (eqan? s1 s2)
+    (or (atom? s1) (atom? s2)) false
+    :else (eqlist? s1 s2)))
+
+(defn rember
+  [s l]
+  (cond
+    (empty? l) '()
+    (equal? (first l) s) (rest l)
+    :else (cons (first l)
+                (rember s (rest l)))))
---
+++ test/the_little_schemer/5_it_is_full_of_stars_test.clj
@@ -0,0 +1,117 @@
+(ns the-little-schemer.5-it-is-full-of-stars-test
+  (:use clojure.test
+        the-little-schemer.5-it-is-full-of-stars))
+
+(deftest test-rember*
+  (testing "リストが空の場合、空のリストを返す"
+    (is (= (rember* 'x '()) '())))
+
+  (testing "ls から a を取り除いた、別のリストを返す"
+    (are [a ls expected]
+      (= (rember* a ls) expected)
+      'cup '((coffee) cup ((tea) cup) (and (hick)) cup)
+      '((coffee) ((tea)) (and (hick)))
+
+      'sauce '(((tomato sauce)) ((bean) sauce) (and ((flying)) sauce))
+      '(((tomato)) ((bean)) (and ((flying)))))))
+
+
+(deftest test-insertR*
+  (testing "リストが空の場合、空のリストを返す"
+    (is (= (insertR* 'x 'y '()) '())))
+
+  (testing "ls 内の old の右に new を挿入した、別のリストを返す"
+    (are [new old ls expected]
+      (= (insertR* new old ls) expected)
+      'roast 'chuck
+      '((how much (wood)) could ((a (wood) chuck)) (((chuck)))
+        (if (a) ((wood chuck))) could chuck wood)
+      '((how much (wood)) could ((a (wood) chuck roast)) (((chuck roast)))
+        (if (a) ((wood chuck roast))) could chuck roast wood))))
+
+
+(deftest test-occur*
+  (testing "リストが空の場合、0 を返す"
+    (is (zero? (occur* 'x '()))))
+
+  (testing "ls 内に出現する a の数を返す"
+    (are [a ls expected]
+      (= (occur* a ls) expected)
+      'banana
+      '((banana) (split ((((banana ice))) (cream (banana)) sherbet))
+        (banana) (bread) (banana brandy))
+      5)))
+
+
+(deftest test-subst*
+  (testing "リストが空の場合、空のリストを返す"
+    (is (= (subst* 'x 'y '()) '())))
+
+  (testing "ls 内に出現する old を new で置き換えた、別のリストを返す"
+    (are [new old ls expected]
+      (= (subst* new old ls) expected)
+      'orange 'banana
+      '((banana) (split ((((banana ice))) (cream (banana)) sherbet))
+        (banana) (bread) (banana brandy))
+      '((orange) (split ((((orange ice))) (cream (orange)) sherbet))
+        (orange) (bread) (orange brandy)))))
+
+
+(deftest test-insertL*
+  (testing "リストが空の場合、空のリストを返す"
+    (is (= (insertL* 'x 'y '()) '())))
+
+  (testing "ls 内に出現する old の左に new を挿入した、別のリストを返す"
+    (are [new old ls expected]
+      (= (insertL* new old ls) expected)
+      'pecker 'chuck
+      '((how much (wood)) could ((a (wood) chuck))
+        (((chuck))) (if (a) ((wood chuck))) could chuck wood)
+      '((how much (wood)) could ((a (wood) pecker chuck))
+        (((pecker chuck))) (if (a) ((wood pecker chuck))) could pecker chuck wood))))
+
+
+(deftest test-member*
+  (testing "リストが空の場合、false を返す"
+    (is (not (member* 'x '()))))
+
+  (testing "ls の中に a が存在する場合、true を返す"
+    (are [a ls]
+      (member* a ls)
+      'chips '((potato) (chips ((with) fish) (chips)))))
+
+  (testing "ls の中に a が存在しない場合、false を返す"
+    (are [a ls]
+      (not (member* a ls))
+      'chicken '((potato) (chips ((with) fish) (chips))))))
+
+
+(deftest test-leftmost
+  ; 空リストを含まない、空でないリストを受けとることを、事前の条件としている
+  (testing "リストの中で一番左のアトムを返す"
+    (are [ls expected]
+      (= (leftmost ls) expected)
+      '((potato) (chips ((with) fish) (chips))) 'potato
+      '(((hot) (tuna (and))) cheese) 'hot)))
+
+
+(deftest test-eqlist?
+  (testing "l1 と l2 の要素が全て同じならば、true を返す"
+    (are [l1 l2]
+      (eqlist? l1 l2)
+      '(strawberry ice cream) '(strawberry ice cream)
+      '(beef ((sausage)) (and (soda))) '(beef ((sausage)) (and (soda)))))
+
+  (testing "l1 と l2 の要素が一つでも異なれば、false を返す"
+    (are [l1 l2]
+      (not (eqlist? l1 l2))
+      '(strawberry ice cream) '(strawberry cream ice)
+      '(banana ((split))) '((banana) (split))
+      '(beef ((sausage)) (and (soda))) '(beef ((salami)) (and (soda))))))
+
+
+(deftest test-rember
+  (testing "l の中で最初に出現する s を取り除き、別のリストを返す"
+    (are [s l expected]
+      (= (rember s l) expected)
+      '((sausage)) '(beef ((sausage)) (and (soda))) '(beef (and (soda))))))
